09c76e49eb93a26153c9383120d426c752e0bd1f
shadowik <zahar.mark@bk.ru>
2022-03-10 23:36:40 +0400

    Settings hover

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.reels)
+        setContentView(R.layout.settings_hover)
         supportActionBar?.hide()
     }
 }
